90d855c76753c6da3babc85f878cccf46e241317
saroj-mali <sarojmali6090@gmail.com>
2024-12-29 18:20:02 +0530

    solved multisearch, and practiced debouncing and throttling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./screens/About.jsx";
-import Contact from "./screens/Contact.jsx";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Home from "./screens/Home.jsx";
-import ThemeProvider from "./context/ThemeContext.jsx";
+import MultiSelectSearch from "./components/MultiSelectSearch.jsx";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <div className="mx-auto w-[800px] border-2 border-gray-200 h-screen">
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
-    </ThemeProvider>
+    <div className="mx-auto w-[800px] border-2 border-gray-200 h-screen">
+      <MultiSelectSearch />
+    </div>
   );
 };
 
---
+++ src/components/MultiSelectSearch.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { RxCross2 } from "react-icons/rx";
+import useDebounce from "../custom-hooks/useDebounce";
+
+const MultiSelectSearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [users, setUsers] = useState([]);
+  const [pills, setPiils] = useState([]);
+
+  const debounceTerm = useDebounce(searchTerm, 500);
+
+  const addPills = (name) => {
+    setPiils([...pills, name]);
+  };
+
+  const removePills = (name) => {
+    const newPills = pills.filter((pill) => pill !== name);
+    setPiils(newPills);
+  };
+
+  const clearPills = () => {
+    setPiils([]);
+  };
+
+  const fetchUsers = async () => {
+    if (!debounceTerm) {
+      setUsers([]);
+      return;
+    }
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/users/search?q=${searchTerm}`
+      );
+      const data = await response.json();
+      setUsers(data.users);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [debounceTerm]);
+
+  return (
+    <div className="relative">
+      <div className="flex relative items-center border-2 border-gray-200 w-full h-12">
+        <div className="flex flex-wrap gap-2 mt-2">
+          {pills.map((pill) => (
+            <div key={pill} className="flex items-center gap-2">
+              <span className="bg-blue-500 text-white px-2 py-1 rounded-full">
+                {pill}
+              </span>
+              <RxCross2 onClick={() => removePills(pill)} />
+            </div>
+          ))}
+        </div>
+        <input
+          type="text"
+          placeholder="Search users"
+          className="border-none outline-none ml-4"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <RxCross2 className="absolute right-5" onClick={() => clearPills()} />
+      </div>
+      <ul className="mt-10 ">
+        {users.length > 0 &&
+          users.map((user) => (
+            <li
+              onClick={() => addPills(user.firstName)}
+              className="text-black"
+              key={user.id}
+            >
+              {user.firstName}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MultiSelectSearch;
